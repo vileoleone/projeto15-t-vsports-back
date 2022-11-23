74a6bb8c35b6f8b3e4a704867b3d9b61940d901d
Victor Leone de Oliveira <vileoleone@gmail.com>
2022-11-22 22:03:44 -0300

    feat: added basic directories of back-api repositore achiteture

---
+++ src/database/db.js
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv"
+
+dotenv.config()
+//setup for MongoDB server
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+try {
+    mongoClient.connect(() => {
+       console.log("connected with database")
+    })
+} catch (error) {
+    console.log(error)
+}
+
+export const db = mongoClient.db()
